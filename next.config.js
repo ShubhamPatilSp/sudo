3ecb2e5e953104d0f99c0fe4f8cd9c896cf6e495
ShubhamPatilSp <xshuubhampatil@gmail.com>
2025-06-24 11:58:01 +0530

    add

---
+++ next.config.js
@@ -0,0 +1,20 @@
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  output: 'export',
+  experimental: {
+    appDir: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*',
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
